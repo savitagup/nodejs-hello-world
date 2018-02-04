6fa141856d570f48e7554051dac722daa0c2981c
Savita Gupta <Savita@Savitas-MacBook-Air.local>
2018-02-04 11:42:50 -0500

    hello world changes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+var PORT = process.PORT;
+
 var express = require('express');
 var app = express();
 
-app.listen(3000,function(){
+app.listen(PORT,function(){
     console.log('server is up and running on port 3000');
 });
 
